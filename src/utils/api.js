69657a504e4d2e09a08bcf36cfb802354beffa25
gayathri <gayathri.dkp@gmail.com>
2026-09-23 14:10:10 +0530

    Update baseURL to live Render backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://shopsphere-backend-wi39.onrender.com/api',
 });
 
 // Automatically attach JWT token to requests if available in localStorage
